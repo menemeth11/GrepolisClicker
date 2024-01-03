7118cf3000a58903212d8ad1c7c30a375c22e663
Szymon Pacyno <szymon.pacyno@gmail.com>
2024-01-04 00:54:05 +0100

    wasting time on grid, new gui

--- login.py
+++ login.py
@@ -1,3 +1,4 @@
+import time
 import tkinter as tk
 import customtkinter as ctk
 
@@ -16,16 +17,37 @@ def NextPage():
 #***    
 #idk = tk.Tk()
 page = ctk.CTk()
-page.geometry("720x360")
+page.geometry("470x360")
 page.title("Login/Register Page")
+page.resizable(False, False)
 
-label = ctk.CTkLabel(page, text="Hi, Login Page")
-label.pack()
+label = ctk.CTkLabel(page, text="HELLO WORLD!", font=("Segoe UI", 50, "bold"), text_color="red", bg_color="yellow").grid(column=0, row=0,columnspan= 2, sticky="we")
+
+menuFrame = ctk.CTkFrame(page, width=450, height=80, border_width=3, border_color="green").grid(column=0, row=1,columnspan= 2,rowspan=2, padx=10, pady=10)
+
+textLogin = ""
+labelLogin = ctk.CTkLabel(menuFrame, text="LOGIN:", text_color="green", font=("Segoe UI", 15, "bold"), bg_color="#2b2b2b").grid(column=0, row=1,sticky="s")
+inputLogin = ctk.CTkEntry(menuFrame, placeholder_text="Put Your login here", width=180, textvariable=textLogin).grid(column=0, row=2, sticky="n")
+
+textPassword = ""
+labelPassword = ctk.CTkLabel(menuFrame, text="PASSWORD:", text_color="green", font=("Segoe UI", 15, "bold"), bg_color="#2b2b2b").grid(column=1, row=1,sticky="s")
+inputPassword = ctk.CTkEntry(menuFrame, placeholder_text="Put Your password here", width=180, show="*", textvariable=textPassword).grid(column=1, row=2, sticky="n")
 
 
-label1 = ctk.CTkLabel(page, text="HELLO WORLD!", font=("Segoe UI", 50, "bold"), text_color="red")
-label1.pack()
 
+
+def login():
+    print(f"Login: {textLogin}, Password: {textPassword}")
+    x = inputPassword.__str__
+    print(x)
+
+buttonLogin = ctk.CTkButton(page, text="Login!", command="login").grid(column=0, columnspan=2)
+
+
+
+
+### MAKE IT AGAIN 
+'''
 frameMenu = ctk.CTkFrame(page, height=200, width=600)
 frameMenu.pack()
 #next
@@ -37,21 +59,21 @@ label2 = ctk.CTkEntry(loginFrame, placeholder_text="PUT LOGIN HERE").pack(pady=1
 passwordsFrame = tk.LabelFrame(frameMenu, text="Password", bd=5, relief="ridge")
 passwordsFrame.grid(row=1, column=0, sticky="W", padx=20, pady=20)
 
-label3 = ctk.CTkEntry(passwordsFrame, placeholder_text="PUT PASSWORD HERE").pack(pady=10)
+label3 = ctk.CTkEntry(passwordsFrame, placeholder_text="PUT PASSWORD HERE").pack(pady=10)'''
 
 
-buttonPrev = ctk.CTkButton(page, text="Previous Page", command=PrevPage, state="disabled")
-buttonPrev.pack(side="left", padx=10, pady=10)
+#buttonPrev = ctk.CTkButton(page, text="Previous Page", command=PrevPage, state="disabled")
+#buttonPrev.pack(side="left", padx=10, pady=10)
 '''
 def Quit():
     answer = messageBox.askyesno(title="Quit Confirm", message="Are you sure you want to quit"
     if answer:
         page.destroy()
 '''
-buttonQuit = ctk.CTkButton(page, text="QUIT", state="disabled").pack(side="left")
+#buttonQuit = ctk.CTkButton(page, text="QUIT", state="disabled").pack(side="left")
 
-buttonNext = ctk.CTkButton(page, text="Next Page", command=NextPage)
-buttonNext.pack(side="right", padx=10, pady=10)
+#buttonNext = ctk.CTkButton(page, text="Next Page", command=NextPage)
+#buttonNext.pack(side="right", padx=10, pady=10)
 
 
 page.mainloop()
